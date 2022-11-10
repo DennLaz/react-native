db162c0cb91d56da33006bb4137b0b024df04034
Denys Lazarenko <dentus25@gmail.com>
2022-11-10 11:09:20 +0200

    Update App.js

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>ПРИВЕТ</Text>
+      <Text>Привет это мой старт в React Native</Text>
       <StatusBar style="auto" />
     </View>
   );
